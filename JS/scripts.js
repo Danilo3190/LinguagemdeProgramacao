364b4147fd6d1a9ad39b22f436d0a782f3b69a3c
your name <danilo3190rodrigues@gmail.com>
2025-02-25 09:06:39 -0300

    Exercicio 1

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,16 @@
 function seguir(){
     alert('voces agora esta seguindo Danilo S Rodrigues')
 }
+//exercicio 1
+function exe1(){
+    //vamos usar Dom para recuperar os dados do HTML
+    //Dom - Document Object Model
+    //Converte Texto para  numero - Number()
+    let nro1 =Number( document.getElementById("nro1").value)
+    let nro2 =Number( document.getElementById("nro2").value)
+    //Vamos fazer uma subtração
+    let sub = nro1 - nro2
+    //vamos mostrar o resultado ao usuario
+    //alert("A subtração é " + sub) 
+    document.getElementById("sub").innerText = "O Resultado é : " + sub;
+}
