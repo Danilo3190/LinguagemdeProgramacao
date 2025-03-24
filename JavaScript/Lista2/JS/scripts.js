a27b74c4627b563c918cd273f34d0fe728262f61
DANILO SILVESTRE RODRIGUES <1090482421017@fatec-srv.local>
2025-03-24 11:04:37 -0300

    meu ultimo commit

--- JavaScript/Lista2/JS/scripts.js
+++ JavaScript/Lista2/JS/scripts.js
@@ -8,10 +8,10 @@ function exe1(){
     //calcular média
     let media =(nota1 + nota2 + nota3 + nota4) / 4
     if(media >= 7){
-        document.getElementById("media").innerText = "Aprovado com media " + media
+        document.getElementById("media").innerHTML = "Aprovado com media " + media
     }
     else{
-        document.getElementById("media").innerText = "Reprovado com media " + media
+        document.getElementById("media").innerHTML =  "Reprovado com media " + media
     }
     }
 function exe2(){
@@ -20,16 +20,16 @@ function exe2(){
     let nota2 = Number(document.getElementById("nota2").value);
     let media = (nota1 + nota2) / 2
     if ((media >=0) && (media < 3)){
-        document.getElementById("media").innerText = "Reprovado" + media
+        document.getElementById("media").innerHTML = "Reprovado :  " + media
     }
     else if ((media >=3) && (media < 7)){
-        document.getElementById("media").innerText = "Exame" + media
+        document.getElementById("media").innerHTML = "Exame :  " + media
     }
     else if ((media >=7) && (media <= 10)){
-        document.getElementById("media").innerText = "Aprovado" + media
+        document.getElementById("media").innerHTML = "Aprovado : " + media
     }
     else{
-        document.getElementById("media").innerText = "Problema com notas" + media
+        document.getElementById("media").innerHTML = "Problema com notas : " + media
 
 }
 }
@@ -38,15 +38,15 @@ function exe3(){
    let numero2 = Number(document.getElementById("numero2").value);
    let menor
    if (numero1 < numero2){
-       document.getElementById("menor").innerText = " O Menor é : " + numero1
+       document.getElementById("menor").innerHTML = " O Menor é : " + numero1
     
     }
      else if  (numero2 < numero1){
-        document.getElementById("menor").innerText = " O Menor é : " + numero2
+        document.getElementById("menor").innerHTML = " O Menor é : " + numero2
           
     }
     else{
-        document.getElementById("menor").innerText = " Os dois são iguais " 
+        document.getElementById("menor").innerHTML = " Os dois são iguais " 
     }
 }
    
@@ -56,13 +56,13 @@ function exe4(){
     let numero2 = Number(document.getElementById("numero2").value);
     let numero3 = Number(document.getElementById("numero3").value);
     if (numero1 >= numero2 && numero1 >= numero3){
-        document.getElementById("maior").innerText = " O maior é : " + numero1
+        document.getElementById("maior").innerHTML = " O maior é : " + numero1
     }
     else if (numero2 >= numero1 && numero2 >= numero3){
-        document.getElementById("maior").innerText = " O maior é : " + numero2
+        document.getElementById("maior").innerHTML = " O maior é : " + numero2
     }
     else{ (numero3 >= numero1 && numero3 >= numero2)
-        document.getElementById("maior").innerText = " O maior é : " + numero3
+        document.getElementById("maior").innerHTML = " O maior é : " + numero3
        
 
       }
@@ -73,24 +73,65 @@ function exe5(){
     let selecao = Number(document.getElementById("selecao").value);
     //ultilizacao  do escolha
     switch(selecao){
-        case 1: document.getElementById("resultado").innerText = ( numero1 + numero2) / 2
+        case 1: document.getElementById("resultado").innerHTML = ( numero1 + numero2) / 2
             break
         case 2: if(numero1 >= numero2){
         }
         else {
-            document.getElementById("resulatdo").innerText = numero1 - numero2
+            document.getElementById("resulatdo").innerHTML = numero1 - numero2
         }
             break
-        case 3: document.getElementById("resulatdo").innerText = numero1 * numero2
+        case 3: document.getElementById("resulatdo").innerHTML = numero1 * numero2
             break
         case 4: if ( numero2 !=0){
-            document.getElementById("resulatdo").innerText = numero1 / numero2
+            document.getElementById("resulatdo").innerHTML = numero1 / numero2
         }  
         else{
-            document.getElementById("resulatdo").innerText = "Divisão por zero"
+            document.getElementById("resulatdo").innerHTML = "Divisão por zero"
         }  
                 
             
     }
+}
+function exe6(){
+    let numero1 = Number(document.getElementById("numero1").value)
+    let numero2 = Number(document.getElementById("numero2").value)
+    let selecao = Number(document.getElementById("selecao").value)
+    switch(selecao){
+        case 1: document.getElementById("resultado").innerHTML= "A potencia é : " +
+        Match.pow(numero1, numero2)
+              break
+        case 2: document.getElementById("resultado").innerHTML = "RQ do numero1 " + Match.sqrt(numero1)
+              break
+        case 3: document.getElementById("resultado").innerHTML = "RC do numero1" + Match.cbrt(numero1)
+        .toFixed(2) + "RC do numero2" + Math.cbrt(numero2).toFixed(2)
+             break
+    }
+}
+function exe7(){
+    let salario = Number(document.getElementById("salario").value)
+    //verificar salario
+    if (salario < 500){
+        document.getElementById("novo").innerHTML = "Novo salario " + (salario + salario*30/100)
+        }
+    else{
+        document.getElementById("novo").innerHTML = "Salario maior ou igual a 500, sem reajuste"
+    }   
+    }
+function exe8(){
+    let salario = Number(document.getElementById("salario").value)
+    //verificar salario
+    if (salario < 0){
+        document.getElementById("novo").innerHTML = "Salario negativo" 
+        }
+    else if (salario <= 300){
+        document.getElementById("novo").innerHTML = "Novo salario" + (salario + salario*15/100)
+       }  
+     else {
+            document.getElementById("novo").innerHTML = "Novo salario" + (salario + salario*15/100)
+        }  
+
+}
+function exe9(){
     
 }
