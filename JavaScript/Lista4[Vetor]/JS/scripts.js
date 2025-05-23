5b213423c42dcfc7843622ffdaa611687c931b69
DANILO SILVESTRE RODRIGUES <1090482421017@fatec-srv.local>
2025-05-23 09:20:53 -0300

    Orientação objeto

--- JavaScript/Lista4[Vetor]/JS/scripts.js
+++ JavaScript/Lista4[Vetor]/JS/scripts.js
@@ -258,56 +258,55 @@ for(let i=0;i<10;i++){//para cada elemento do vetor 1
 alert(`Vetor resulatado 2 ${vettorr2}`) //adicinar contador no vetor2
  }
 function exe11(){
-    //cadastrar codigo e qtd em estoque
-    let vetor = []
-    for(let i=0;i<3;i++){
-        //cria objeto
-        let objeto = {
-            codigo: prompt(`Informe o codigo do Produto`),
-            estoque:Number(prompt(`Informe a qtd em estoque `)),
-            preco: Number(prompt(`Informe o preço do produto`))
-        }
-        //adicionar objeto no vetor
-        vetor.push(objeto)
-    }
-    //comprando...
-    let cliente = Number(prompt(`Informe codigo do cliente`))
-    do {
-        let codigo = Number (prompt(`Informe codigo do produto para compra`))
-        let qtd = Number(prompt(`Informe a qtd do produto para compra`))
-        //procura pra ver se o produto existe
-        let produtoEncontrado
-        let i
-        for(i=0;i<3;i++){
-            if (vetor[i],codigo == codigo){//produto encontrado
-                produtoEncontrado = vetor[i]//guarda produtoe em produtoEncontrado
-                break; //para de procurar - sair do for
-            }
-            if (i == 3){//produto nao existe
-                alert(`codigo ${codigo} inexistente`);
-            }
-            else{//produto existe
-                //tem estoque suficiente
-                if (produtoEncontrado.estoque -= qtd) {
-                    //atualiza o estoque
-                    produtoEncontrado.estoque -= qtd;
-                    alert(`Pedido atendido. Obrigado e volte sempre`)
-                }
-                else{
-                    alert(`Não temos estoque suficiente desta Mercadoria`)
-                }
-            }
-            cliente = Number(prompt(`Informe codigo do novo cliente. Digite 0 para encerrar`))
-          }  //fecha o do
-          while (cliente != 0){
-            console.log(vetor);
-        }
-    
-    
-    }
-}
+    //cadastrar codigo e qtd em estoque//orinetação objeto
+     let vetor = []
+     for(let i=0; i<3;i++){
+         // cria objeto
+         let objeto = {
+             codigo: prompt(`Informe o código do produto`),
+             estoque: Number(prompt(`Informe a qtde em estoque`)),
+             preco: Number(prompt(`Informe o preço do produto`))
+         }
+         // adiciona objeto no vetor
+         vetor.push(objeto)
+     }
+     // comprando ...
+     let cliente = Number(prompt(`Informe código do cliente`))
+     do {
+         let codigo = prompt(`Informe código do produdo para compra`)
+         let qtde = Number(prompt(`Informe a qtde do produto para compra`))
+         // procura pra ver se o produto existe
+         let produtoEncontrado
+         let i
+         for(i=0;i<3;i++){
+             if (vetor[i].codigo == codigo){ // produtor encontrado
+                 produtoEncontrado = vetor[i] // guarda produto em produtoEncontrado
+                 break // pára de procurar - sair do for
+             }
+         }
+         if (i == 3){ // produto não existe
+             alert(`Código ${codigo} inexistente`)
+         }
+         else { // produto existe
+             // tem em estoque suficiente
+             if ( produtoEncontrado.estoque >= qtde){
+                 // atualiza o estoque
+                 produtoEncontrado.estoque -= qtde
+                 alert(`Pedido atendido. Obrigado e volte sempre`)
+             }
+             else { 
+                 alert(`Não temos estoque suficiente desta mercadoria`)
+             }
+         }
+         cliente = Number(prompt(`Informe código do novo cliente. Digite 0 para encerrar`))
+     } // fecha o do
+     while (cliente != 0)
+     console.log(vetor)
+ }
 
-function exe12(){
+
+
+function exe12(){//orinetacão objeto
     let vetor = []
     // entrada de dados
     for(let i=0;i<3;i++){
@@ -350,6 +349,35 @@ function exe12(){
     alert(`${objMaior.nome} vai receber maior comissão de ${objMaior.comissao}`)
     alert(`${objMenor.nome} vai receber menor comissão de ${objMenor.comissao}`)
 }
+function exe13(){//orientação obejto
+        let vetor = []
+        for(let i=0;i<10;i++){
+            let obj = {
+                nome : prompt(`Informe nome do produto ${i+1}`),
+                codigo : Number(prompt(`Informe código do produto ${i+1}`)),
+                preco : Number(prompt(`Informe preço do produto ${i+1}`)),
+                novo: 0
+            }
+            vetor.push(obj)
+        }
+        // gerar o relatório com os novos preços
+        for(let i=0;i<10;i++){
+            if (vetor[i].codigo % 2 == 0 && vetor[i].preco > 1000){
+                vetor[i].novo = vetor[i].preco + vetor[i].preco * 20/100
+            }
+            else if (vetor[i].codigo % 2 == 0){
+                vetor[i].novo = vetor[i].preco + vetor[i].preco * 15/100
+            }
+            else if (vetor[i].preco > 100){
+                vetor[i].novo = vetor[i].preco + vetor[i].preco * 10/100
+            }
+            else {
+                vetor[i].novo = vetor[i].preco
+            }
+            alert(`${vetor[i].nome} - ${vetor[i].codigo} - ${vetor[i].preco} - ${vetor[i].novo}`)
+        }
+    }
+
 
 
 
