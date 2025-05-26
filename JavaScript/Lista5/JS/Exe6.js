0496a98e957de0705d2ea463fb96018426daa615
DANILO SILVESTRE RODRIGUES <1090482421017@fatec-srv.local>
2025-05-26 10:54:39 -0300

    dividir pra conquistra

---
+++ JavaScript/Lista5/JS/Exe6.js
@@ -0,0 +1,43 @@
+function exe12(){//orinetacão objeto
+    let vetor = []
+    // entrada de dados
+    for(let i=0;i<3;i++){
+        let objeto = {
+        nome:(prompt(`Informe nome do ${i+1}o vendedor`)),
+        vendas:(Number(prompt(`Informe total vendido pelo ${i+1}o vendedor`))),
+        percentual:(Number(prompt(`Informe percentual de vendas do ${i+1}o vendedor`)))
+        }
+       
+        // calcula a comissão // orientacao obj
+        objeto.comissao =(objeto.vendas * objeto.percentual) / 100
+        //add o obj no vetor
+        vetor.push(objeto)
+    }
+  
+    // relação dos vendedores e os valores a receber
+    let relatorio = ''
+    for(let i=0;i<3;i++){
+        relatorio += `\nVendedor: ${nomes[i].nome} vai receber ${vetor[i].comissao}`
+    }
+    alert(relatorio)
+    // total das vendas de todos os vendedores
+    let somaVendas = 0
+    for(let i=0;i<3;i++){
+        somaVendas = somaVendas + vetor[i].vendas
+    }
+    alert(`Total de vendas ${somaVendas.toFixed(2)}`)
+    // calcula o maior e menor valor de comissão
+    let objMaior = vetor[0] // primeiro é o maior
+    let objMenor = vetor[0] // primeiro é o menor
+    for(let i=1;i<3;i++){
+        if (vetor[i].comissao > Maior.comissao){
+            Maior = vetor[i]
+        }
+        if (vetor[i] < Menor.comissao){
+            Menor = vetor[i]
+        }
+    }
+   
+    alert(`${objMaior.nome} vai receber maior comissão de ${objMaior.comissao}`)
+    alert(`${objMenor.nome} vai receber menor comissão de ${objMenor.comissao}`)
+}
---
+++ JavaScript/Lista5/JS/Funcao2.js
@@ -0,0 +1,15 @@
+function entrada(va){
+}
+function processa(va){
+    va = va + 10
+}
+function saida(va){
+    alert(va)
+}
+function main(){
+    //passsado um numero como parametro da função
+    // va vai receber  o valor da a (passagem de parametro por valor)
+    entrada(a)
+    processa(a)
+    saida(a)
+}
